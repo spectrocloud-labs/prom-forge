d6153c36333b24504054d5627b28e1731bf43496
Steve Freed <steven.freed@spectrocloud.com>
2026-05-05 09:28:41 -0400

    fix: remove Type method for metrics

--- internal/metrics/counter/counter.go
+++ internal/metrics/counter/counter.go
@@ -11,24 +11,20 @@ import (
 
 // CounterMetric is the main struct for a counter metric.
 type CounterMetric struct {
-	name    string
-	pattern Pattern
-	labels  map[string]string
+	name       string
+	pattern    Pattern
+	labels     map[string]string
+	metricType writev2.Metadata_MetricType
 }
 
 // New creates a new CounterMetric.
 func New(name string, pattern Pattern, labels map[string]string) *CounterMetric {
-	return &CounterMetric{name: name, labels: labels, pattern: pattern}
+	return &CounterMetric{name: name, labels: labels, pattern: pattern, metricType: writev2.Metadata_METRIC_TYPE_COUNTER}
 }
 
 // Name returns the name of the counter metric.
 func (c *CounterMetric) Name() string {
 	return c.name
-}
-
-// Type returns the type of the counter metric.
-func (c *CounterMetric) Type() writev2.Metadata_MetricType {
-	return writev2.Metadata_METRIC_TYPE_COUNTER
 }
 
 // Next returns the next value of the counter metric.
@@ -38,5 +34,5 @@ func (c *CounterMetric) Next(timestamp time.Time) float64 {
 
 // RemoteWrite writes the counter metric to Prometheus.
 func (c *CounterMetric) RemoteWrite(ctx context.Context, client *remote.API, metricValue float64, timestamp time.Time) error {
-	return metrics.RemoteWrite(ctx, client, c.name, c.labels, c.Type(), metricValue, timestamp)
+	return metrics.RemoteWrite(ctx, client, c.name, c.labels, c.metricType, metricValue, timestamp)
 }
--- internal/metrics/gauge/gauge.go
+++ internal/metrics/gauge/gauge.go
@@ -11,24 +11,20 @@ import (
 
 // GaugeMetric is the main struct for a gauge metric.
 type GaugeMetric struct {
-	name    string
-	pattern Pattern
-	labels  map[string]string
+	name       string
+	pattern    Pattern
+	labels     map[string]string
+	metricType writev2.Metadata_MetricType
 }
 
 // New creates a new GaugeMetric.
 func New(name string, pattern Pattern, labels map[string]string) *GaugeMetric {
-	return &GaugeMetric{name: name, labels: labels, pattern: pattern}
+	return &GaugeMetric{name: name, labels: labels, pattern: pattern, metricType: writev2.Metadata_METRIC_TYPE_GAUGE}
 }
 
 // Name returns the name of the gauge metric.
 func (g *GaugeMetric) Name() string {
 	return g.name
-}
-
-// Type returns the type of the gauge metric.
-func (g *GaugeMetric) Type() writev2.Metadata_MetricType {
-	return writev2.Metadata_METRIC_TYPE_GAUGE
 }
 
 // Next returns the next value of the gauge metric.
@@ -38,5 +34,5 @@ func (g *GaugeMetric) Next(timestamp time.Time) float64 {
 
 // RemoteWrite writes the gauge metric to Prometheus.
 func (g *GaugeMetric) RemoteWrite(ctx context.Context, client *remote.API, metricValue float64, timestamp time.Time) error {
-	return metrics.RemoteWrite(ctx, client, g.name, g.labels, g.Type(), metricValue, timestamp)
+	return metrics.RemoteWrite(ctx, client, g.name, g.labels, g.metricType, metricValue, timestamp)
 }
